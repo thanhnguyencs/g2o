49e4fe7b97c73b60c60201a913b4cb71381e6daf
Thanh Nguyen <thanh.nguyen@icg.tugraz.at>
2014-06-21 12:57:34 +0200

    Add comments.

--- g2o/core/matrix_operations.h
+++ g2o/core/matrix_operations.h
@@ -44,11 +44,12 @@ namespace g2o {
       y.segment(yoff, A.rows()) += A * x.segment<Eigen::Matrix<double, Eigen::Dynamic, t>::ColsAtCompileTime>(xoff);
     }
 
-    //template<>
-    /*inline void axpy(const Eigen::MatrixXd& A, const Eigen::Map<const Eigen::VectorXd>& x, int xoff, Eigen::Map<Eigen::VectorXd>& y, int yoff)
-    {
-      y.segment(yoff, A.rows()) += A * x.segment(xoff, A.cols());
-    }*/
+    // duplicate with above, causing issues with VS2013
+    //template<> 
+    //inline void axpy(const Eigen::MatrixXd& A, const Eigen::Map<const Eigen::VectorXd>& x, int xoff, Eigen::Map<Eigen::VectorXd>& y, int yoff)
+    //{
+    //  y.segment(yoff, A.rows()) += A * x.segment(xoff, A.cols());
+    //}
 
     template<typename MatrixType>
     inline void atxpy(const MatrixType& A, const Eigen::Map<const Eigen::VectorXd>& x, int xoff, Eigen::Map<Eigen::VectorXd>& y, int yoff)
@@ -62,11 +63,12 @@ namespace g2o {
       y.segment<Eigen::Matrix<double, Eigen::Dynamic, t>::ColsAtCompileTime>(yoff) += A.transpose() * x.segment(xoff, A.rows());
     }
 
+    // duplicate with above, causing issues with VS2013
     //template<>
-    /*inline void atxpy(const Eigen::MatrixXd& A, const Eigen::Map<const Eigen::VectorXd>& x, int xoff, Eigen::Map<Eigen::VectorXd>& y, int yoff)
-    {
-      y.segment(yoff, A.cols()) += A.transpose() * x.segment(xoff, A.rows());
-    }*/
+    //inline void atxpy(const Eigen::MatrixXd& A, const Eigen::Map<const Eigen::VectorXd>& x, int xoff, Eigen::Map<Eigen::VectorXd>& y, int yoff)
+    //{
+    //  y.segment(yoff, A.cols()) += A.transpose() * x.segment(xoff, A.rows());
+    //}
 
   } // end namespace internal
 } // end namespace g2o
